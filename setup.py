49efc739b06d33407e550631d138e015ba648b4a
Alexander Loechel <Alexander.Loechel@lmu.de>
2014-08-07 16:18:24 +0200

    first stepes of theme building

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ version = '1.0'
 setup(name='lmu.theme.intranet',
       version=version,
       description="LMU Theme for ZUV-Intranet",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -20,8 +20,9 @@ setup(name='lmu.theme.intranet',
       author_email='Alexander.Loechel@lmu.de',
       url='https://github.com/loechel/lmu.theme.intranet',
       license='GPLv2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['lmu'],
+      packages=find_packages('src',exclude=['ez_setup']),
+      package_dir={'': 'src'},
+      namespace_packages=['lmu', 'lmu.theme'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
