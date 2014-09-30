c464e6e85966bc140e1555f25711d917088380ed
Alexander Loechel <AlexanderLoechel@yahoo.de>
2014-10-01 00:55:25 +0200

    update theme

--- src/lmu/theme/intranet/diazo_resources/static/main.js
+++ src/lmu/theme/intranet/diazo_resources/static/main.js
@@ -4,3 +4,42 @@
 /*global jQuery:false, document:false, window:false, location:false */
 
 console.log('main.js loaded');
+
+jQuery(function($){
+/*
+    $('#blog-footer a').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            formselector: 'form#login_form',
+            cssclass: 'overlay-login',
+            noform: function () {
+                if (location.href.search(/pwreset_finish$/) >= 0) {
+                    return 'redirect';
+                } else {
+                    return 'reload';
+                }
+            },
+            redirect: function () {
+                var href = location.href;
+                if (href.search(/pwreset_finish$/) >= 0) {
+                    return href.slice(0, href.length-14) + 'logged_in';
+                } else {
+                    return href;
+                }
+            }
+        }
+    );
+*/
+    $('#blog-footer a').prepOverlay(
+        {
+            subtype: 'ajax',
+            filter: common_content_filter,
+            cssclass: 'overlay-contact',
+            formselector: 'form[name="feedback_form"]',
+            noform: function(el) {return $.plonepopups.noformerrorshow(el, 'close');}
+        }
+    );
+
+});
+
