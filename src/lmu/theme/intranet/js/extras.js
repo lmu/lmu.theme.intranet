12c9092b7a11bb909f599e599d38f2989997a3f7
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-05-11 09:45:37 +0200

    js tricks

--- src/lmu/theme/intranet/js/extras.js
+++ src/lmu/theme/intranet/js/extras.js
@@ -1,1 +1,9 @@
 /* extras.js */
+
+
+$("#pinnwand").load("https://iukintest.verwaltung.uni-muenchen.de/functions/pinnwand/frontpage_view.include");
+
+if ($("#blog-person").length > 0) {
+	var ck = $("#blog-person").text()
+	$("#blog-person").load("https://iukintest.verwaltung.uni-muenchen.de/functions/blog-mit/frontpage_view.include?author="+ck);
+}
