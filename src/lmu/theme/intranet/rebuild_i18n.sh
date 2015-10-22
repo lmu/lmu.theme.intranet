458890dc6e1a7666ffa83a9fc0d302a04af48602
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-10-22 11:22:45 +0200

    translation for intranet

--- src/lmu/theme/intranet/rebuild_i18n.sh
+++ src/lmu/theme/intranet/rebuild_i18n.sh
@@ -9,4 +9,5 @@ i18ndude rebuild-pot --exclude resources --pot locales/${PRODUCTNAME}.pot --merg
 i18ndude sync --pot locales/${PRODUCTNAME}.pot locales/*/LC_MESSAGES/${PRODUCTNAME}.po
 
 i18ndude sync --pot locales/plone.pot locales/*/LC_MESSAGES/plone.po
+i18ndude sync --pot locales/lmu.contenttypes.polls.pot locales/*/LC_MESSAGES/lmu.contenttypes.polls.po
 
